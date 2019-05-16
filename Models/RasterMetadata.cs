ec26c3c0bd3953a8bde79f49345a76ed7ecd34d2
gisvlasta <vasilis.vlastaras@gmail.com>
2019-05-16 09:53:30 +0100

    raster metadata

---
+++ Models/RasterMetadata.cs
@@ -0,0 +1,38 @@
+﻿
+#region Imported Namespaces
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace TellUsToolkit.GHIA.RasterConvert.Models {
+
+  /// <summary>
+  /// The metadata of the raster.
+  /// </summary>
+  public class RasterMetadata {
+
+    private int _totalOutputColumns;
+
+    /// <summary>
+    /// The output 
+    /// </summary>
+    public int OutputRasterColumns {
+      get {
+        return _totalOutputColumns;
+      }
+      set {
+        _totalOutputColumns = value;
+      }
+    }
+
+
+
+
+  }
+
+}
